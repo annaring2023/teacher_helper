4e82afe527b19767f4d5e48dbd01bdd0abd45dce
nyb-arch <vus.solomia.2012@gmail.com>
2025-07-25 17:10:31 +0300

    pdf

--- ok.py
+++ ok.py
@@ -1,24 +1,25 @@
 
 
 from fpdf import FPDF
- 
-# save FPDF() class into 
-# a variable pdf
-pdf = FPDF()   
- 
-# Add a page
-pdf.add_page()
- 
-# set style and size of font 
-# that you want in the pdf
-pdf.set_font("Arial", size = 15)
+def create_pdf(txt_file_name:str) -> None:
+    
+    # save FPDF() class into 
+    # a variable pdf
+    pdf = FPDF()   
+    
+    # Add a page
+    pdf.add_page()
+    
+    # set style and size of font 
+    # that you want in the pdf
+    pdf.set_font("Arial", size = 15)
 
-# open the text file in read mode
-f = open("math_multiply", "r")
+    # open the text file in read mode
+    f = open(txt_file_name, "r")
 
-# insert the texts in pdf
-for x in f:
-    pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
- 
-# save the pdf with name .pdf
-pdf.output("mygfg.pdf")
+    # insert the texts in pdf
+    for x in f:
+        pdf.cell(200, 10, txt = x, ln = 1, align = 'L')
+    
+    # save the pdf with name .pdf
+    pdf.output(txt_file_name)
